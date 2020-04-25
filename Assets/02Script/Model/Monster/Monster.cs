348160eb92a4c5c024a54556cc396cbc3f36f1e3
SungyuJang <gkdlzhtk@gmail.com>
2020-04-25 10:35:14 +0900

    roamin, attack, follow

--- Assets/02Script/Model/Monster/Monster.cs
+++ Assets/02Script/Model/Monster/Monster.cs
@@ -44,6 +44,7 @@ public class Monster : Model
             {
                 case State.roaming: StartCoroutine(DoRoaming()); break;
                 case State.following: StartCoroutine(DoFollowing()); break;
+                case State.attack: StartCoroutine(DoAttack()); break;
             }
         }
     }
@@ -54,7 +55,6 @@ public class Monster : Model
         float nowRoamingDistance = 0f;
         while(BeForeState == State.roaming)
         {
-            yield return new WaitForSeconds(2f);
             nowRoamingDistance = 0f;
             transform.eulerAngles = new Vector3(0f, Random.Range(-180f, 180f), 0f);
 
@@ -74,19 +74,46 @@ public class Monster : Model
                     NowState = State.following;
                     break;
                 }
+                print(nowRoamingDistance);
             }
             Rigidbody.velocity = Vector3.zero;
+            yield return new WaitForSeconds(3f);
         }
     }
     IEnumerator DoFollowing()
     {
         while (BeForeState == State.following) 
         {
+            yield return new WaitForFixedUpdate();
             transform.LookAt(Character.transform.position);
-
             Rigidbody.velocity = transform.forward * SPD;
+
+            if (IsOutRoamingArea)
+            {
+                NowState = State.roaming;
+                break;
+            }
+            else if(IsCloseEnoughWithChracter)
+            {
+                Rigidbody.velocity = Vector3.zero;
+                NowState = State.attack;
+                break;
+            }
         }
-        yield return null;
+    }
+
+    IEnumerator DoAttack()
+    {
+        while(BeForeState == State.attack)
+        {
+            yield return new WaitForFixedUpdate();
+            transform.LookAt(Character.transform.position);
+            if (!IsCloseEnoughWithChracter)
+            {
+                NowState = State.following;
+                break;
+            }
+        }
     }
     bool IsOutRoamingArea { get { return !RoamingArea.Contains(GMath.ConvertV3xzToV2(transform.position)); } }
     bool IsDetectedCharacter { get { 
@@ -98,6 +125,9 @@ public class Monster : Model
                 }
             } 
             return false; } }
+    bool IsCloseEnoughWithChracter { get {
+            return Vector3.Distance(Character.transform.position, transform.position) <= 1f;
+        } }
     protected void OnDrawGizmos()
     {
         
