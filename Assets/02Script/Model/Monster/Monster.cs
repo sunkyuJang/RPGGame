5f88281ce75288e212cba0defc9379e32d3ab872
JangSungyu <gkdlzhtk@gmail.com>
2020-04-30 14:36:06 +0900

    MonsterAnimator
    Need Attack MotionTime Change

--- Assets/02Script/Model/Monster/Monster.cs
+++ Assets/02Script/Model/Monster/Monster.cs
@@ -9,7 +9,7 @@ public class Monster : Model
     Character Character { set; get; }
     RaycastHit raycastHit = new RaycastHit();
     protected Rect RoamingArea { set; get; }
-    protected enum State { roaming, following, battle, attack, getHit, Dead, non }
+    protected enum State { roaming, following, battle, attack, getHit, dead, idle}
     protected State NowState { set; get; }
     protected State BeForeState { set; get; }
     Coroutine stateProcess;
@@ -23,8 +23,7 @@ public class Monster : Model
     {
         base.Awake();
         NowState = State.roaming;
-        BeForeState = State.non;
-        print(RoamingArea);
+        BeForeState = State.idle;
     }
 
     protected void MonsterSetInfo(Rect roamingArea) 
@@ -50,6 +49,8 @@ public class Monster : Model
             BeForeState = NowState;
             switch (NowState)
             {
+                case State.idle: StartCoroutine(DoIdle(true)); break;
+                case State.battle: StartCoroutine(DoIdle(false)); break;
                 case State.roaming: StartCoroutine(DoRoaming()); break;
                 case State.following: StartCoroutine(DoFollowing()); break;
                 case State.attack: StartCoroutine(DoAttack()); break;
@@ -58,15 +59,41 @@ public class Monster : Model
     }
     IEnumerator DoRoaming()
     {
-        float nowRoamingDistance = 0f;
-        float roamingDistance = 5f;
-        float pauseTime = 0f;
-        float pauseTimeLimit = 3f;
+        DoAnimator(State.following);
+        float roamingDistance = 0f;
+        float roamingDistanceLimit = 5f;
+/*        float pauseTime = 0f;
+        float pauseTimeLimit = 3f;*/
+        transform.eulerAngles = new Vector3(0f, Random.Range(-180f, 180f), 0f);
+        Rigidbody.velocity = transform.forward * SPD;
+        
         while(BeForeState == State.roaming)
         {
-            transform.eulerAngles = new Vector3(0f, Random.Range(-180f, 180f), 0f);
+            if(roamingDistance < roamingDistanceLimit)
+            {
+                roamingDistance += SPD * Time.fixedDeltaTime;
+            }
+            else
+            {
+                Rigidbody.velocity = Vector3.zero;
+                NowState = State.idle;
+                yield break;
+            }
+
+            yield return new WaitForFixedUpdate();
             
-            while (pauseTime < pauseTimeLimit) 
+            if (IsOutRoamingArea)
+            {
+                transform.LookAt(GMath.ConvertV2ToV3xz(RoamingArea.center));
+                Rigidbody.velocity = transform.forward * SPD;
+            }
+            else if (IsDetectedCharacter)
+            {
+                NowState = State.following;
+                yield break;
+            }
+
+            /*while (pauseTime < pauseTimeLimit) 
             {
                 if (IsOutRoamingArea)
                 {
@@ -83,17 +110,43 @@ public class Monster : Model
                 Rigidbody.velocity = nowRoamingDistance < roamingDistance
                     ? transform.forward * SPD : Vector3.zero;
 
+                DoAnimator(Rigidbody.velocity != Vector3.zero ? State.following : State.idle);
+
                 pauseTime += Rigidbody.velocity != Vector3.zero
                     ? 0f : Time.fixedDeltaTime;
 
                 yield return new WaitForFixedUpdate();
+            }*/
+        }
+    }
+    IEnumerator DoIdle(bool isNomalIdle)
+    {
+        DoAnimator(isNomalIdle ? State.idle : State.battle);
+        float pauseTime = 0f;
+        float pauseTimeLimit = isNomalIdle ? 3f : 1f;
+        while(BeForeState == State.idle)
+        {
+            if (pauseTime < pauseTimeLimit)
+            {
+                pauseTime += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+
+                if (IsDetectedCharacter)
+                {
+                    NowState = State.following;
+                    yield break;
+                }
             }
-            pauseTime = 0f;
-            nowRoamingDistance = 0f;
+            else
+            {
+                NowState = State.roaming;
+                yield break;
+            }
         }
     }
     IEnumerator DoFollowing()
     {
+        DoAnimator(State.following);
         while (BeForeState == State.following) 
         {
             yield return new WaitForFixedUpdate();
@@ -107,6 +160,7 @@ public class Monster : Model
             }
             else if(IsCloseEnoughWithChracter)
             {
+                DoAnimator(State.battle);
                 Rigidbody.velocity = Vector3.zero;
                 NowState = State.attack;
                 break;
@@ -117,16 +171,27 @@ public class Monster : Model
 
     IEnumerator DoAttack()
     {
-        while(BeForeState == State.attack)
+        DoAnimator(State.attack);
+        float pauseTime = 0f;
+        float pauseTimeLimit = 1f;
+        while (BeForeState == State.attack)
         {
             yield return new WaitForFixedUpdate();
+
             transform.LookAt(Character.transform.position);
+
             if (!IsCloseEnoughWithChracter)
             {
                 NowState = State.following;
                 break;
             }
+            
+            if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.98f)
+            {
+
+            }
         }
+        print("isOut");
     }
     void DoDead()
     {
@@ -138,7 +203,19 @@ public class Monster : Model
         nowHP -= Damege - DEF;
         if(nowHP <= 0)
         {
-            NowState = State.Dead;
+            NowState = State.dead;
+        }
+    }
+
+    void DoAnimator(State state)
+    {
+        switch (state)
+        {
+            case State.idle: Animator.SetTrigger("NomalIdle"); break;
+            case State.following: Animator.SetTrigger("Walking"); break;
+            case State.battle: Animator.SetTrigger("BattleIdle"); break;
+            case State.attack: Animator.SetTrigger("Attack"); break;
+            case State.getHit: Animator.SetTrigger("GetHit"); break;
         }
     }
 
@@ -160,7 +237,6 @@ public class Monster : Model
                         RaycastHit hit = new RaycastHit();
                         if (Physics.Raycast(transform.position + Vector3.up, CharacterDirection, out hit, Vector3.Distance(transform.position, Character.transform.position)))
                         {
-                            print("isIn");
                             if (hit.collider.CompareTag("Character"))
                             {
                                 return true;
@@ -169,23 +245,13 @@ public class Monster : Model
                     }
                 }
             }
+
             return false; 
         } 
     }
     bool IsCloseEnoughWithChracter { get {
             return Vector3.Distance(Character.transform.position, transform.position) <= 1f;
         } }
-
-/*    public void DoAnimation(State state) 
-    {
-        switch (state)
-        {
-            case State.roaming:  break;
-            case State.following: break;
-            case State.roaming:break;
-            case State.roaming:break;
-        }
-    }*/
     protected void OnDrawGizmos()
     {
         Gizmos.color = new Color(1,1,1, 0.5f);
