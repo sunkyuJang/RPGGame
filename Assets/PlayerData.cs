7ca6058355327d88d119d0f355ed6cd731273fe6
sunkyuJang <gkdlzhtk@gmail.com>
2020-09-21 07:31:43 +0900

    BossSceneFix

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,30 @@
+﻿using GLip;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class PlayerData
+{
+    public string ID { set; get; }
+    public string NickName { set; get; }
+    public Vector3 LastStandPosition { set; get; }
+    public List<ItemManager.ItemCounter> InventoryItemList { set; get; }
+        = new List<ItemManager.ItemCounter>();
+
+    public List<ItemManager.ItemCounter> EquipmentItems { set; get; }
+        = new List<ItemManager.ItemCounter>();
+
+    public PlayerData(string id, string nickName)
+    {
+        ID = id;
+        NickName = nickName;
+    }
+
+    public void CreateJsonFile()
+    {
+        var fileStream = new FileStream(string.Format("{0}/{1}.json", Application.dataPath, ID), FileMode.Create);
+    }
+}
