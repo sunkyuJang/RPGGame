50599096c08d38001201e465965d311fa9e0759a
sunkyuJang <gkdlzhtk@gmail.com>
2020-09-21 14:15:16 +0900

    LoginUIComplete

---
+++ Assets/02Script/System/Login/LoginManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public Text title;
+
+    public Button signUpBtn;
+    public Button loginBtn;
+    public InputField idField;
+    public InputField pwField;
+
+    List<PlayerData> playerDatas = new List<PlayerData>();
+
+    string gameName = "Proto";
+    string singUpAcc = "SingUp";
+    string loginText = "Login";
+    string cancelText = "Cancel";
+    private void Awake()
+    {
+        signUpBtn.onClick.AddListener(CreatAccount);
+        loginBtn.onClick.AddListener(CheckAccount);
+    
+        
+    }
+
+    public void CheckAccount()
+    {
+        string id = idField.textComponent.text;
+        string pw = pwField.textComponent.text;
+
+
+    }
+    public void CreatAccount()
+    {
+        title.text = singUpAcc;
+        signUpBtn.onClick.AddListener(ConfirmCreatingAccount);
+
+        loginBtn.transform.GetChild(0).GetComponent<Text>().text = cancelText;
+        loginBtn.onClick.AddListener(CancelCreatingAccount);
+        
+    }
+    public void ConfirmCreatingAccount()
+    {
+        string id = idField.textComponent.text;
+        string pw = pwField.textComponent.text;
+
+        print(PlayerData.path);
+        PlayerData nowData = new PlayerData(id, "jk");
+        //File.WriteAllText(PlayerData.path + id + ".json", JsonUtility.ToJson(data));
+        print(JsonUtility.ToJson(nowData, true));
+        print(nowData.ID);
+    }
+
+    public void CancelCreatingAccount()
+    {
+        title.text = gameName;
+        loginBtn.transform.GetChild(0).GetComponent<Text>().text = loginText;
+        loginBtn.onClick.AddListener(CheckAccount);
+    }
+
+    [SerializeField]
+    public class PlayerData
+    {
+        public string id;
+        public string pw;
+        public string NickName;
+
+        public static string path = Application.dataPath + "/Resources/Managers/SaveData/";
+        public PlayerData(string id, string nickName)
+        {
+            ID = id;
+            NickName = nickName;
+        }
+
+        public void CreateJsonFile()
+        {
+            var fileStream = new FileStream(string.Format("{0}/{1}.json", Application.dataPath, ID), FileMode.Create);
+        }
+    }
+}
--- Assets/02Script/System/Login/PlayerData.cs
+++ Assets/02Script/System/Login/PlayerData.cs
@@ -1,4 +1,5 @@
 ﻿using GLip;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,17 +7,13 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.XR.WSA.Input;
 
-public class PlayerData
+//[Serializable]
+/*public class PlayerData
 {
     public string ID { set; get; }
     public string NickName { set; get; }
-    public Vector3 LastStandPosition { set; get; }
-    public List<ItemManager.ItemCounter> InventoryItemList { set; get; }
-        = new List<ItemManager.ItemCounter>();
 
-    public List<ItemManager.ItemCounter> EquipmentItems { set; get; }
-        = new List<ItemManager.ItemCounter>();
-
+    public static string path = Application.dataPath + "/Resources/Managers/SaveData/";
     public PlayerData(string id, string nickName)
     {
         ID = id;
@@ -27,4 +24,4 @@ public class PlayerData
     {
         var fileStream = new FileStream(string.Format("{0}/{1}.json", Application.dataPath, ID), FileMode.Create);
     }
-}
+}*/
